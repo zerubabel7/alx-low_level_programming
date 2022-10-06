daa71d4e2be7f7d7fd29458c6b9e5ee7f3b76df0
zerubabel <zerubabel.zerihun0973@gmail.com>
2022-10-06 00:26:33 -0700

    task 0 - 3

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * array_range - creates an array of intigers.
+ * @min: minimum value
+ * @max: maximum value
+ *
+ * Retrun: pointer to newlycreated array.
+ * NULL if malloc fails.
+ * Null if min > max.
+ */
+int *array_range(int min, int max)
+{
+	int range, i;
+	int *p;
+
+	range = 0;
+
+	if (min > max)
+		return (NULL);
+
+	range = ((max + 1) - min);
+
+	p = malloc(range * sizeof(int));
+
+	if (p == NULL)
+		return (NULL);
+
+	for (i = 0; i < range; i++)
+	{
+		*(p + i) = min + i;
+	}
+
+	return (p);
+}
---
+++ 0x0C-more_malloc_free/_putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: character to print
+ *
+ * Return: on success 1
+ * on error, -1 is returned, and error is set appropriatley
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
