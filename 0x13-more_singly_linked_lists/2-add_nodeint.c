00ea8fc33566c2be8b57ceea6cd65b16e0a39f1f
zerubabel <zerubabel.zerihun0973@gmail.com>
2022-10-25 00:24:51 -0700

    hotness

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -12,7 +12,7 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	listint_t *new_node;
 
 	new_node = malloc(sizeof(listint_t));
-	if (new_node != Null)
+	if (new_node != NULL)
 	{
 		new_node->n = n;
 		new_node->next = *head;
